5119cf7bdc713f9a24c735b8a8e60258fa3afd74
SiXiang <siyijiang233@qq.com>
2025-04-26 19:22:56 +0800

    图表分析

--- src/views/charts/baifen.vue
+++ src/views/charts/baifen.vue
@@ -1,23 +1,106 @@
 <template>
   <div class="chart-container">
-    <chart height="100%" width="100%" />
+    <div ref="chart" style="height: 100%; width: 100%;" />
   </div>
 </template>
 
 <script>
-import Chart from '@/components/Charts/baifen'
+import * as echarts from 'echarts'
 
 export default {
   name: 'Baifen',
-  components: { Chart }
+  data() {
+    return {
+      chart: null,
+      chartData: {
+        currentYear: [120, 132, 101, 134, 90, 230, 210, 182, 191, 234, 290, 330],
+        lastYear: [100, 120, 110, 115, 85, 195, 180, 160, 170, 210, 250, 300]
+      }
+    }
+  },
+  mounted() {
+    this.initChart()
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$refs.chart)
+
+      const option = {
+        tooltip: {
+          trigger: 'axis',
+          formatter: params => {
+            return `${params[0].name}<br/>
+              ${params[0].marker} ${params[0].seriesName}: ${params[0].value}人<br/>
+              ${params[1].marker} ${params[1].seriesName}: ${params[1].value}人`
+          }
+        },
+        legend: {
+          data: ['2023年', '2022年'],
+          top: 'bottom'
+        },
+        xAxis: {
+          type: 'category',
+          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+          axisLabel: {
+            rotate: 45
+          }
+        },
+        yAxis: {
+          type: 'value',
+          axisLabel: {
+            formatter: '{value} 人'
+          }
+        },
+        series: [
+          {
+            name: '2023年',
+            type: 'bar',
+            data: this.chartData.currentYear,
+            itemStyle: {
+              color: '#5470C6'
+            },
+            emphasis: {
+              focus: 'series'
+            }
+          },
+          {
+            name: '2022年',
+            type: 'bar',
+            data: this.chartData.lastYear,
+            itemStyle: {
+              color: '#91CC75'
+            },
+            emphasis: {
+              focus: 'series'
+            }
+          }
+        ],
+        grid: {
+          top: '10%',
+          left: '3%',
+          right: '4%',
+          bottom: '12%',
+          containLabel: true
+        }
+      }
+
+      this.chart.setOption(option)
+    },
+    handleResize() {
+      this.chart && this.chart.resize()
+    }
+  }
 }
 </script>
 
 <style scoped>
-.chart-container{
+.chart-container {
   position: relative;
   width: 100%;
   height: calc(100vh - 84px);
 }
 </style>
-
--- src/views/charts/line.vue
+++ src/views/charts/line.vue
@@ -1,23 +1,154 @@
 <template>
   <div class="chart-container">
-    <chart height="100%" width="100%" />
+    <div ref="chart" style="height: 100%; width: 100%;" />
   </div>
 </template>
 
 <script>
-import Chart from '@/components/Charts/LineMarker'
+import * as echarts from 'echarts'
 
 export default {
   name: 'LineChart',
-  components: { Chart }
+  data() {
+    return {
+      chart: null,
+      chartData: {
+        // 示例数据结构
+        2022: {
+          domestic: [120, 132, 101, 134, 90, 230, 210, 182, 191, 234, 290, 330],
+          overseas: [80, 72, 99, 66, 110, 70, 90, 118, 109, 66, 110, 70]
+        },
+        2023: {
+          domestic: [150, 142, 121, 154, 100, 250, 230, 202, 211, 254, 300, 350],
+          overseas: [90, 82, 109, 76, 120, 80, 100, 128, 119, 86, 120, 80]
+        }
+      }
+    }
+  },
+  computed: {
+    processedData() {
+      const years = ['2022', '2023']
+      const months = Array.from({ length: 12 }, (_, i) => `${i + 1}月`)
+
+      return {
+        xAxis: months,
+        series: years.flatMap(year => [
+          {
+            year,
+            type: 'domestic',
+            data: this.chartData[year].domestic,
+            total: this.chartData[year].domestic.map((v, i) => v + this.chartData[year].overseas[i])
+          },
+          {
+            year,
+            type: 'overseas',
+            data: this.chartData[year].overseas,
+            total: this.chartData[year].domestic.map((v, i) => v + this.chartData[year].overseas[i])
+          }
+        ])
+      }
+    }
+  },
+  mounted() {
+    this.initChart()
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$refs.chart)
+
+      const option = {
+        tooltip: {
+          trigger: 'axis',
+          formatter: params => {
+            const year = params[0].seriesName.split(' ')[0]
+            const month = params[0].name
+            const total = this.processedData.series
+              .find(s => s.year === year && s.type === 'domestic')
+              .total[params[0].dataIndex]
+
+            return params.map(p => {
+              const percent = ((p.value / total) * 100).toFixed(1)
+              return `${year}年${month}<br/>
+                ${p.marker} ${p.seriesName.split(' ')[1]}: ${p.value}人 (${percent}%)`
+            }).join('<br/>')
+          }
+        },
+        legend: {
+          data: ['2022 境内', '2022 境外', '2023 境内', '2023 境外'],
+          top: 'bottom'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.processedData.xAxis,
+          axisLabel: {
+            rotate: 45
+          }
+        },
+        yAxis: {
+          type: 'value',
+          axisLabel: {
+            formatter: '{value} 人'
+          }
+        },
+        series: [
+          // 2022系列
+          {
+            name: '2022 境内',
+            type: 'bar',
+            stack: '2022',
+            data: this.chartData[2022].domestic,
+            itemStyle: { color: '#5470C6' }
+          },
+          {
+            name: '2022 境外',
+            type: 'bar',
+            stack: '2022',
+            data: this.chartData[2022].overseas,
+            itemStyle: { color: '#9ac8ff' }
+          },
+          // 2023系列
+          {
+            name: '2023 境内',
+            type: 'bar',
+            stack: '2023',
+            data: this.chartData[2023].domestic,
+            itemStyle: { color: '#91CC75' },
+            barGap: '30%'
+          },
+          {
+            name: '2023 境外',
+            type: 'bar',
+            stack: '2023',
+            data: this.chartData[2023].overseas,
+            itemStyle: { color: '#c2e3a9' }
+          }
+        ],
+        grid: {
+          top: '10%',
+          left: '3%',
+          right: '4%',
+          bottom: '20%',
+          containLabel: true
+        }
+      }
+
+      this.chart.setOption(option)
+    },
+    handleResize() {
+      this.chart && this.chart.resize()
+    }
+  }
 }
 </script>
 
 <style scoped>
-.chart-container{
+.chart-container {
   position: relative;
   width: 100%;
   height: calc(100vh - 84px);
 }
 </style>
-
